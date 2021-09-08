838fdc6ae11f395c287c25e1ee03de14846bf875
Lea <lea.dukaez@gmail.com>
2021-09-08 21:45:15 +0200

    Initial Commit

---
+++ City Sights/City_SightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  City_SightsApp.swift
+//  City Sights
+//
+//  Created by Léa Dukaez on 08/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct City_SightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ City Sights/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  City Sights
+//
+//  Created by Léa Dukaez on 08/09/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
